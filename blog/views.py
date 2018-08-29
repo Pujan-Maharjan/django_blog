da1baa68fd6d55222c59c42ad444ba2b00b6aecd
Pujan Maharjan <puzan2072@gmail.com>
2018-08-29 19:51:20 +0545

    define api views for our post api model

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from blog.models import Post
+from blog.serializers import PostSerializer
+
 
 # Create your views here.
+
+@api_view(['GET', 'POST'])
+def post_list(request):
+
+    """
+    List latest 10 post lists, or create a post
+    """
+    if request.method == 'GET':
+        posts = Post.objects.all()[:10]
+        serializer = PostSerializer(posts, many = True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = PostSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def post_detail(request, pk):
+    """
+    Retrieve, update or remove specific post
+    """
+    try:
+        post = Post.objects.get(pk = pk)
+    except Post.DoesNotExist:
+        return Response(status = status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = PostSerializer(data = request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        post.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
+
+
+    
